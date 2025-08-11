e63450d5a1cc0ae45cc0b18223119234d438dc86
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2025-08-11 12:24:50 +0200

    cycle through copilot suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,6 +6,8 @@ return {
       -- Optional: Configure Copilot keybindings or settings
       vim.g.copilot_no_tab_map = true
       vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.api.nvim_set_keymap("i", "<C-n>", '<Plug>(copilot-next)', { silent = true })
+      vim.api.nvim_set_keymap("i", "<C-p>", '<Plug>(copilot-previous)', { silent = true })
     end
   }
 }
